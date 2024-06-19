ba0cf732179f45d5fc2d78f13949694dee04e4e8
Nick Piaddo <nick.piaddo@protonmail.com>
2024-06-19 08:14:25 +0000

    Create enum `monitor_status`

--- src/core/fs/mod.rs
+++ src/core/fs/mod.rs
@@ -12,8 +12,10 @@ pub use file_lock_struct::FileLock;
 pub use file_system_enum::FileSystem;
 pub use fs_type_enum::FsType;
 pub use monitor_kind_enum::MonitorKind;
+pub use monitor_status_enum::MonitorStatus;
 
 mod file_lock_struct;
 mod file_system_enum;
 mod fs_type_enum;
 mod monitor_kind_enum;
+mod monitor_status_enum;
---
+++ src/core/fs/monitor_status_enum.rs
@@ -0,0 +1,17 @@
+// Copyright (c) 2023 Nick Piaddo
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// From dependency library
+
+// From standard library
+
+// From this library
+
+/// Status of a `MountTableMonitor`.
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
+#[non_exhaustive]
+pub enum MonitorStatus {
+    ChangeDetected,
+    TimeOut,
+    Error,
+}
