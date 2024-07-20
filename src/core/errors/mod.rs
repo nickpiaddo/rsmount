86abd1865887f1e74957a839654a30c01b1dbfae
Nick Piaddo <nick.piaddo@protonmail.com>
2024-07-20 07:27:57 +0000

    Create module `core::errors`

--- src/core/errors/mod.rs
+++ src/core/errors/mod.rs
@@ -1,11 +1,10 @@
 // Copyright (c) 2023 Nick Piaddo
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! `rsmount` low-level API.
+//! Runtime errors.
 
 // From dependency library
 
 // From standard library
 
 // From this library
-pub mod errors;
