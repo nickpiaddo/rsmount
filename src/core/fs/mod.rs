208b48d30118bbce51e0c89dd4e4981ef1ba124d
Nick Piaddo <nick.piaddo@protonmail.com>
2024-07-20 06:59:34 +0000

    Create module `core::fs`

--- src/core/fs/mod.rs
+++ src/core/fs/mod.rs
@@ -1,13 +1,10 @@
 // Copyright (c) 2023 Nick Piaddo
 // SPDX-License-Identifier: Apache-2.0 OR MIT
 
-//! `rsmount` low-level API.
+//! Module for working with file systems.
 
 // From dependency library
 
 // From standard library
 
 // From this library
-pub mod device;
-pub mod errors;
-pub mod fs;
