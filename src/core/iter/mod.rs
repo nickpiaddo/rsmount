f1121598aefc5d7cf1d2b0505f091a8518b121fb
Nick Piaddo <nick.piaddo@protonmail.com>
2024-07-25 12:11:22 +0000

    Create struct `swaps_iter`

--- src/core/iter/mod.rs
+++ src/core/iter/mod.rs
@@ -13,9 +13,11 @@ pub use fs_tab_iter_mut_struct::FsTabIterMut;
 pub use fs_tab_iter_struct::FsTabIter;
 pub use gen_iterator_struct::GenIterator;
 pub use mount_info_iter_struct::MountInfoIter;
+pub use swaps_iter_struct::SwapsIter;
 
 mod direction_enum;
 mod fs_tab_iter_mut_struct;
 mod fs_tab_iter_struct;
 mod gen_iterator_struct;
 mod mount_info_iter_struct;
+mod swaps_iter_struct;
