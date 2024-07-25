dc9b2abbf47d2ad65375153d6f394871f5567558
Nick Piaddo <nick.piaddo@protonmail.com>
2024-07-25 10:24:32 +0000

    Create struct `fs_tab_iter_mut`

--- src/core/iter/mod.rs
+++ src/core/iter/mod.rs
@@ -9,9 +9,11 @@
 
 // From this library
 pub use direction_enum::Direction;
+pub use fs_tab_iter_mut_struct::FsTabIterMut;
 pub use fs_tab_iter_struct::FsTabIter;
 pub use gen_iterator_struct::GenIterator;
 
 mod direction_enum;
+mod fs_tab_iter_mut_struct;
 mod fs_tab_iter_struct;
 mod gen_iterator_struct;
