5a57e5bb5dc8a83e873787d96af39e009cb3de3f
Nick Piaddo <nick.piaddo@protonmail.com>
2024-08-05 10:31:52 +0000

    Create struct `mount_info_overmount_iter`

---
+++ src/core/iter/mount_info_overmount_iter_struct.rs
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Nick Piaddo
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// From dependency library
+
+// From standard library
+
+// From this library
+use crate::gen_overmount_iter;
+
+gen_overmount_iter!(MountInfo, MountInfoEntry);
