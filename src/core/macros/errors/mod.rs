68a553e623805418449193ea423f4bf8690b17b8
Nick Piaddo <nick.piaddo@protonmail.com>
2024-08-06 12:16:45 +0000

    Create macro `gen_diff_iter_error`

--- src/core/macros/errors/mod.rs
+++ src/core/macros/errors/mod.rs
@@ -7,6 +7,7 @@
 
 // From this library
 mod gen_child_iter_error_macro;
+mod gen_diff_iter_error_macro;
 mod gen_table_diff_error_macro;
 mod gen_table_entry_error_macro;
 mod gen_table_iter_error_macro;
