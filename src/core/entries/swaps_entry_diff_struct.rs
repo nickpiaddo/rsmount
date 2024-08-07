3ff3c3c80e9eeca0f8daba3568366e32685ff543
Nick Piaddo <nick.piaddo@protonmail.com>
2024-08-07 06:00:50 +0000

    Create struct `swap_entry_diff`

---
+++ src/core/entries/swaps_entry_diff_struct.rs
@@ -0,0 +1,11 @@
+// Copyright (c) 2023 Nick Piaddo
+// SPDX-License-Identifier: Apache-2.0 OR MIT
+
+// From dependency library
+
+// From standard library
+
+// From this library
+use crate::gen_entry_diff;
+
+gen_entry_diff!(Swaps, SwapsEntry);
