9496a4bdffb914871be86072d39f97476e773589
Nick Piaddo <nick.piaddo@protonmail.com>
2025-05-28 13:26:14 +0000

    Relocate macros `owning_ref/owning_mut`

--- src/core/macros/tables/mod.rs
+++ src/core/macros/tables/mod.rs
@@ -7,4 +7,3 @@
 
 // From this library
 mod gen_table_diff_macro;
-mod table_shared_methods_macro;
--- src/ffi_utils.rs
+++ src/ffi_utils.rs
@@ -17,6 +17,38 @@ use std::str::Utf8Error;
 
 // From this library
 
+#[allow(unused_macros)]
+#[macro_export]
+#[doc(hidden)]
+macro_rules! owning_ref_from_ptr {
+    ($object_ref:expr, $output_ref_ty:ident, $ptr: ident) => {{
+        let mut obj_ptr = std::ptr::NonNull::from($object_ref.as_ref());
+        let boxed = Box::new($ptr);
+        let (boxed_ptr, item) = unsafe { <$output_ref_ty>::ref_from_boxed_ptr(boxed) };
+
+        // Adds boxed pointer to garbage collector
+        unsafe { obj_ptr.as_mut().gc.push(boxed_ptr.into()) };
+
+        item
+    }};
+}
+
+#[allow(unused_macros)]
+#[macro_export]
+#[doc(hidden)]
+macro_rules! owning_mut_from_ptr {
+    ($object_ref:expr, $output_ref_ty:ident, $ptr: ident) => {{
+        let mut obj_ptr = std::ptr::NonNull::from($object_ref.as_ref());
+        let boxed = Box::new($ptr);
+        let (boxed_ptr, item) = unsafe { <$output_ref_ty>::mut_from_boxed_ptr(boxed) };
+
+        // Adds boxed pointer to garbage collector
+        unsafe { obj_ptr.as_mut().gc.push(boxed_ptr.into()) };
+
+        item
+    }};
+}
+
 //---- Conversion functions
 
 #[doc(hidden)]
